6f85e6690593f5c99b690179231954eeb16ac616
ToKoAFC <tomaszkrupa95@gmail.com>
2017-10-03 22:20:16 +0200

    add Access and core models

---
+++ AccessLayer/Typer.Database.Access/Access/TeamAccess.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Typer.CoreModels.Models;
+using Typer.Database.Migrations;
+using Typer.Database.Models;
+
+namespace Typer.Database.Access.Access
+{
+    public class TeamAccess
+    {
+        private TyperContext _context;
+        public TeamAccess(TyperContext context)
+        {
+            _context = context;
+        }
+
+        public void AddTeam(CoreTeam coreTeam)
+        {
+            var dbTeam = new DbTeam
+            {
+                TeamId = coreTeam.TeamId,
+                TeamName = coreTeam.TeamName
+            };
+            _context.DbTeams.Add(dbTeam);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ LogicLayer/Typer.CoreModels/Models/CoreTeam.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typer.CoreModels.Models
+{
+    public class CoreTeam
+    {
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+    }
+}
