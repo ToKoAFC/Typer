c2889229541016dcb574363e762fadde4a158a45
ToKoAFC <tomaszkrupa95@gmail.com>
2017-10-03 21:03:21 +0200

    Add db projects

---
+++ AccessLayer/Typer.Database.Migrations/TyperContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Typer.Database.Models.Models;
+
+namespace Typer.Database.Migrations
+{
+    public class TyperContext : DbContext
+    {
+        public DbSet<DbTeam> DbTeams { get; set; }
+    }
+}
---
+++ AccessLayer/Typer.Database.Models/Models/DbTeam.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typer.Database.Models.Models
+{
+    public class DbTeam
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        
+    }
+}
