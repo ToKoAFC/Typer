a10162689900f2142f6ed97db2bde06c86a87da9
ToKoAFC <tomaszkrupa95@gmail.com>
2017-12-02 22:15:10 +0100

    crtl r g

--- LogicLayer/Typer.Services/Services/ProfileService.cs
+++ LogicLayer/Typer.Services/Services/ProfileService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Typer.CoreModels.Models;
+﻿using Typer.CoreModels.Models;
 using Typer.Database.Access;
 using Typer.Services.Interfaces;
 using Typer.ViewModels.Common;
